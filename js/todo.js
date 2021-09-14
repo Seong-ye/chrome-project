2c5b8ffb414a410b86cefcc50a3c830032c3aee2
seongye Park <sungye24@naver.com>
2021-09-14 21:55:03 +0900

    third commit

--- js/todo.js
+++ js/todo.js
@@ -1,7 +1,6 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
-
 const TODOS_KEY = "todos";
 
 let toDos = [];
